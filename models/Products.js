66162576d3cfc5a91223cc2af57cf5b26c422011
Joaquin Duarte <joaquinduarte1998@gmail.com>
2023-06-07 19:01:51 -0300

    agrega categoriaPadre

--- models/Products.js
+++ models/Products.js
@@ -13,6 +13,10 @@ const ProductSchema = Schema({
         type: String,
         required: true
     },
+    categoriaPadre:{
+        type:String,
+        required:true
+    },
     categoria: {
         type: String,
         required: true
