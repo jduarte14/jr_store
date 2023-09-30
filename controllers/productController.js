b32b37b977cdf66e9dfe1f98a3796d4c3e3ca1b5
Joaquin Duarte <joaquinduarte1998@gmail.com>
2023-09-29 22:26:05 -0300

    update term

--- controllers/productController.js
+++ controllers/productController.js
@@ -50,7 +50,7 @@ const createProduct = async (req, res) => {
         return res.status(200).json({
             status: "success",
             message: "Product created successfully",
-            created_product:product
+            data:product
         });
     } catch (error) {
         return res.status(500).json({
