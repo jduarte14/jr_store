81e44b08ad8a4a91ae31a7a8e7d208d15f772a5c
Joaquin Duarte <joaquinduarte1998@gmail.com>
2023-10-04 16:48:50 -0300

    update v4

--- index.js
+++ index.js
@@ -24,8 +24,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin:whitelist
- }));
+    origin:(origin,callback)=>{
+        const accepted_origins = [
+            'https://dehierroymadera.com.uy'
+        ]
+        if(accepted_origins.includes(origin) || !origin){
+            callback(null,true)
+       }
+       else {
+        return callback(new Error("No allowed by CORS"));
+       }
+    }
+}))
 
 const product_routes = require("./routes/product_routes");
 const user_routes = require("./routes/user_routes");
