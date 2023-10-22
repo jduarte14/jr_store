bdb619012295fac4127a234b44f2ca0b137ac8f5
Joaquin Duarte <joaquinduarte1998@gmail.com>
2023-10-22 20:43:41 -0300

    Banners logic

--- index.js
+++ index.js
@@ -26,10 +26,11 @@ app.use(cors({
 
 const product_routes = require("./routes/product_routes");
 const user_routes = require("./routes/user_routes");
+const banner_routes = require('./routes/banner_routes');
 
 app.use("/api", product_routes);
 app.use("/auth", user_routes);
-
+app.use("/api", banner_routes);
 app.listen(port, () => {
     console.log(`servidor funcionando en el puerto ${port}`);
 });
---
+++ models/Banners.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const BannerSchema = Schema({
+    name:{
+        type:String,
+        required: true,
+    },
+    type:{
+        type:String,
+         required: true,
+    },
+    title:{
+        type:String,
+        required: true,
+    },
+    description:{
+        type:String,
+       required: true,
+    },
+    link:{
+        type:String,
+    },
+    desktop_image:{
+        type:String,
+    },
+    mobile_image:{
+        type:String,
+    }
+});
+
+module.exports = model('banner', BannerSchema, 'banners');
